718f44c64d55c44539b613db06d3e31033de5bf4
osamaiskandarani@gmail.com <osamaiskandarani@gmail.com>
2022-10-30 22:08:17 +0200

    Update my_btree.cpp

--- Assignment 2/my_btree.cpp
+++ Assignment 2/my_btree.cpp
@@ -5,9 +5,9 @@
 /*
 TEST CASES
 
-TEST CASE 1: Empty Tree with no root node	=> checked
-TEST CASE 2: Tree with only the root node	=> checked
-TEST CASE 3: Tree with a root and two direct children (left & right)	=> checked
+TEST CASE 1: Empty Tree with no root node	=> checked (add root)
+TEST CASE 2: Tree with only the root node	=> checked (add to left)
+TEST CASE 3: Tree with a root and two direct children (left & right)	=> checked (add to left)
 TEST CASE 4: Tree with only left children	=> checked (Impossible due to requested method description)
 TEST CASE 5: Tree with only right children	=> checked (Impossible due to requested method description)
 TEST CASE 6: Tree with only left and right children (Inverse V shape)	=> checked
@@ -39,7 +39,11 @@ class binaryTree
 {
 private:
 	treeNode<T>* root;
+	/*
+	REQUIRES: int or char input value
 
+	EFFECTS: add node to the left in case there were two children , one at the right or none at all, add to right in case there was one child at the left
+	*/
 	treeNode<T>* addNode(T value, treeNode<T>* current)
 	{
 		if (current == nullptr)
@@ -52,6 +56,11 @@ private:
 			current->left = addNode(value, current->left);
 		return current;
 	}
+		/*
+	REQUIRES: nothing
+
+	EFFECTS: print preorder
+	*/
 	void printPreOrder(treeNode<T>* root)
 	{
 		if (root == NULL)
@@ -60,6 +69,11 @@ private:
 		printPreOrder(root->left);
 		printPreOrder(root->right);
 	}
+		/*
+	REQUIRES: nothing
+
+	EFFECTS: print postorder
+	*/
 	void printPostOrder(treeNode<T>* root)
 	{
 		if (root == NULL)
@@ -68,6 +82,11 @@ private:
 		printPostOrder(root->right);
 		std::cout << root->data << std::endl;
 	}
+		/*
+	REQUIRES: nothing
+
+	EFFECTS: print inorder
+	*/
 	void printInOrder(treeNode<T>* root)
 	{
 		if (root == NULL)
@@ -76,6 +95,11 @@ private:
 		std::cout << root->data << std::endl;
 		printInOrder(root->right);
 	}
+		/*
+	REQUIRES: nothing
+
+	EFFECTS: return tree size as an int
+	*/
 	int treeSize(treeNode<T>* root)
 	{
 		if (root == NULL)
@@ -83,6 +107,11 @@ private:
 		else
 			return 1 + (treeSize(root->left) + treeSize(root->right));
 	}
+		/*
+	REQUIRES: char or int input value
+
+	EFFECTS: return subtreesize beginning at the input value
+	*/
 	int subTreeSize(treeNode<T>* root, T value, bool check)
 	{
 		if (root == NULL)
@@ -97,6 +126,11 @@ private:
 		else if (root->data > value)
 			return subTreeSize(root->left, value, check);
 	}
+		/*
+	REQUIRES: char or int value
+
+	EFFECTS: delete node at specified value if it has no children, else inform the user that it cannot be deleted
+	*/
 	treeNode<T>* deleteNode(treeNode<T>* current, T value)
 	{
 		if (current == NULL)
@@ -113,6 +147,11 @@ private:
 		return current;
 	}
 public:
+	/*
+	REQUIRES: nothing
+
+	EFFECTS: initialize root to NULL
+	*/
 	binaryTree()
 	{
 		root = NULL;
